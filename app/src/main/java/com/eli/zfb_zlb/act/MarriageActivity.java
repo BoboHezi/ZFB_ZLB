047d0716fa9683bea6ef51b2c6dd54a3d86e87e5
EliChang <eliflichang@gmail.com>
2020-12-13 23:35:47 +0800

    add act: Marriage

---
+++ app/src/main/java/com/eli/zfb_zlb/act/MarriageActivity.java
@@ -0,0 +1,19 @@
+package com.eli.zfb_zlb.act;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.eli.zfb_zlb.R;
+import com.eli.zfb_zlb.util.Utils;
+
+public class MarriageActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Utils.setColor(this, 0xffb0b0b0);
+
+        setContentView(R.layout.activity_marriage);
+    }
+}
